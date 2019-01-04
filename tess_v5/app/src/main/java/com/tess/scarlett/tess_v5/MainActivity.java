7f228e8e4dc016a99a4e817bf9e4659c76581eaf
jiehao <jiehao.liu@mail.utoronto.ca>
2019-01-04 18:45:14 -0500

    bottom navibar

--- tess_v5/app/src/main/java/com/tess/scarlett/tess_v5/MainActivity.java
+++ tess_v5/app/src/main/java/com/tess/scarlett/tess_v5/MainActivity.java
@@ -50,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_camera:
+                    startCameraActivity();
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -67,16 +67,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        final Button button = findViewById(R.id.button_cam);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View c){
-                startCameraActivity();
-            }
-        });
     }
 
 
