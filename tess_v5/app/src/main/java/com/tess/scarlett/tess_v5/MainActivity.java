b7724a40c4bde32084bc2565345c9bfc13c2c5b1
jiehao <jiehao.liu@mail.utoronto.ca>
2019-01-06 16:30:49 -0500

    textView -> mTextMessage

--- tess_v5/app/src/main/java/com/tess/scarlett/tess_v5/MainActivity.java
+++ tess_v5/app/src/main/java/com/tess/scarlett/tess_v5/MainActivity.java
@@ -200,7 +200,7 @@ public class MainActivity extends AppCompatActivity{
             Bitmap bitmap = BitmapFactory.decodeFile(imageUri.getPath(),options);
             String result = this.getText(bitmap);
             System.out.println("return string is "+result +"\n");
-            textView.setText(result);
+            mTextMessage.setText(result);
         } catch (Exception e){
             Log.e(TAG, e.getMessage());
         }
